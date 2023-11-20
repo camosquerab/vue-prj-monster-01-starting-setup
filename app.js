4183f1a2bf4e0373d23cb4d0b42d2b4c71663451
Camilo Alfonso Mosquera Benavides <adrianricardo.rojas.contractor.cidentity@bbva.com>
2023-11-20 03:17:56 -0500

    Implements attack and special attack

---
+++ app.js
@@ -0,0 +1,44 @@
+function getRandomValue(min, max){
+    return Math.floor(Math.random()*(max-min))+min;
+}
+
+const app = Vue.createApp({
+    data(){
+        return{
+            playerHealth: 100,
+            monsterHealth: 100,
+            currentRound: 0,
+        };
+    },
+    computed:{
+        monsterBarStyles(){
+            return {width: this.monsterHealth + '%'};
+        },
+        playerBarStyles(){
+            return {width: this.playerHealth + '%'};
+        },
+        mayUseSpecialAttack(){
+            return this.currentRound %3 !== 0;
+        },
+    },
+    methods:{
+        attackMonster(){
+            this.currentRound++;
+            const atackValue = getRandomValue(5,12);
+            this.monsterHealth -= atackValue;
+            this.attackPlayer();
+        },
+        attackPlayer(){
+            const atackValue = getRandomValue(8,12);
+            this.playerHealth -= atackValue;
+        },
+        specialAttackMonster(){
+            this.currentRound++;
+            const atackValue = getRandomValue(10,25);
+            this.monsterHealth -= atackValue;
+            this.attackPlayer();
+        },
+        
+    }
+});
+app.mount('#game');
